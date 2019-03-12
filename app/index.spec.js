6770ac9142d0a6c9b43e98192f7eeac817de0a99
gargleyark <myself@gargleyark.com>
2019-03-12 21:13:22 +0000

    quick test for index

--- app/index.spec.js
+++ app/index.spec.js
@@ -1,7 +1,16 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+jest.mock('react-dom', () => ({ render: jest.fn() }));
+global.document.getElementById = jest.fn();
+
+import index from './index.js';
+import App from './components/App/App';
+
 describe('Ost Front-end Code Test', () => {
   describe('Index', () => {
-    it('should do something', () => {
-      expect(true).toBe(true);
+    it('renders without crashing', () => {
+      expect(ReactDOM.render).toHaveBeenCalledWith(<App />, null);
     });
   });
 });
